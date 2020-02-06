1b0a92673a1da3e7e1770a8811f65c7f8a8fb068
abouzar <abouzar.mohiti"yahoo.com>
2020-02-07 00:59:20 +0330

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppHeader/>
+    <nuxt class="paddingRuter"/>
+  </div>
+</template>
+
+
+<script>
+  import AppHeader from '~/components/AppHeader.vue'
+
+  export default {
+     components: {
+       AppHeader
+    }
+  }
+</script>
+
+
+<style>
+  .paddingRuter{
+    padding-top: 150px;
+  }
+</style>
